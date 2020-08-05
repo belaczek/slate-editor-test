7b7df4dc6083b5e383aaae80b375ae0152b1c46c
Tomas Belada <tomas@belada.net>
2020-08-05 15:21:44 +0200

    init

--- src/Editor.tsx
+++ src/Editor.tsx
@@ -113,7 +113,7 @@ export class SlateEditor extends Component {
   }
 }
 
-const toggleBlock = (editor: any, format: string) => {
+const toggleBlock = (editor: Editor, format: string) => {
   const isActive = isBlockActive(editor, format);
   const isList = LIST_TYPES.includes(format);
 
@@ -132,7 +132,7 @@ const toggleBlock = (editor: any, format: string) => {
   }
 };
 
-const toggleMark = (editor: any, format: string) => {
+const toggleMark = (editor: Editor, format: string) => {
   const isActive = isMarkActive(editor, format);
 
   if (isActive) {
@@ -171,7 +171,7 @@ const MarkButton: FC<{ format: string }> = ({ format }) => {
   );
 };
 
-const isBlockActive = (editor: any, format: string) => {
+const isBlockActive = (editor: Editor, format: string) => {
   const [match] = Editor.nodes(editor, {
     match: n => n.type === format
   });
@@ -179,7 +179,7 @@ const isBlockActive = (editor: any, format: string) => {
   return !!match;
 };
 
-const isMarkActive = (editor: any, format: string) => {
+const isMarkActive = (editor: Editor, format: string) => {
   const marks = Editor.marks(editor);
   return marks ? marks[format] === true : false;
 };
